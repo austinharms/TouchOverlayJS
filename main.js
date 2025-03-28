b152cce8d42673944d48d67034516713f6870cb7
Austin <austin@harmsonline.com>
2025-03-28 10:51:28 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 const overlay = {};
 overlay.inject = function() {
   alert("Script Inject");
+  const canvas_wrapper = document.getElementById("bgreplay");
+  const canvas = canvas_wrapper.querySelector("canvas")
+  canvas.addEventListener("touchstart", (e) => {
+    alert("Touch Start");
+  });
 };
 
 return overlay;
